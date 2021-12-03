01146a0bff9a9d20aff5135a598bac14fcd279a3
andurebel <andu.trica@gmail.com>
2021-12-03 16:39:18 +0200

    added shuffledCards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-function App() {
+import { cardImages } from "./cardImages.js";
+import { useState } from "react";
+
+export const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({
+        ...card,
+        id: Math.random(),
+      }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
   return (
     <div className="mx-auto flex flex-col h-screen w-full bg-blue-300 items-center justify-center max-w-lg">
       <h1 className="text-3xl font-bold mb-8">Magic Match</h1>
-      <button className="border-2 border-white px-4 py-2 text-white font-bold cursor-pointer rounded hover:bg-red-400 hover:text-white">
+      <button
+        onClick={shuffleCards}
+        className="border-2 border-white px-4 py-2 text-white font-bold cursor-pointer rounded hover:bg-red-400 hover:text-white"
+      >
         New Game
       </button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/cardImages.js
@@ -0,0 +1,8 @@
+export const cardImages = [
+  { src: "/img/minion1.png" },
+  { src: "/img/minion2.png" },
+  { src: "/img/minion3.png" },
+  { src: "/img/minion4.png" },
+  { src: "/img/minion5.png" },
+  { src: "/img/minion6.png" },
+];
